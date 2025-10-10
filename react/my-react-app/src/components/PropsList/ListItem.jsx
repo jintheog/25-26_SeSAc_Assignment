d11fd877b566b39395f798e13f61cfeb4b1a69e5
stanstanstanlee <stanstanstanlee@gmail.com>
2025-10-10 14:46:25 +0900

    react

--- react/my-react-app/src/components/PropsList/ListItem.jsx
+++ react/my-react-app/src/components/PropsList/ListItem.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function ListItem() {
-  return <div>ListItem</div>;
+export default function ListItem({ item }) {
+  return (
+    <div>
+      {item.name}
+      <button>클릭</button>
+    </div>
+  );
 }
